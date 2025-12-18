d0c3b40618d32c827d7e9c65fc1a415888063808
Mike <mmmbeer@gmail.com>
2025-12-17 20:03:39 -0500

    sql to docs

---
+++ shared/types/game.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GamePhase = void 0;
+var GamePhase;
+(function (GamePhase) {
+    GamePhase["LOBBY"] = "LOBBY";
+    GamePhase["MORNING"] = "MORNING";
+    GamePhase["AFTERNOON"] = "AFTERNOON";
+    GamePhase["SELLING"] = "SELLING";
+    GamePhase["ENDED"] = "ENDED";
+})(GamePhase || (exports.GamePhase = GamePhase = {}));
